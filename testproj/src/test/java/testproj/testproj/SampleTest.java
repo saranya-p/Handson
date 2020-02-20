196e1b632a7fd87e85a5f515cdb443550a6759b0
DELL <DELL@DELL>
2020-02-20 11:08:47 +0530

    added a sample class

---
+++ testproj/src/test/java/testproj/testproj/SampleTest.java
@@ -0,0 +1,12 @@
+package testproj.testproj;
+
+import org.junit.Test;
+
+public class SampleTest {
+
+	@Test
+	public void SampleMethod()
+	{
+		System.out.print("This is a sample Test Method");
+	}
+}
